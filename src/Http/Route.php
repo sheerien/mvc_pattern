a76314ca0c462e43826c04a2657e2bd4210636b1
sheerien <freemebabaj@gmail.com>
2021-11-08 19:15:01 +0300

    ADD: put & delete method in route system

--- src/Http/Route.php
+++ src/Http/Route.php
@@ -56,7 +56,29 @@ class Route
     }
 
     /**
-     * Summary of resolve
+     *  put method
+     * @param mixed $route
+     * @param mixed $action
+     * @return void
+     */
+    public static function put($route, $action)
+    {
+        self::$routes['put'][$route] = $action;
+    }
+
+    /**
+     * delete method
+     * @param mixed $route
+     * @param mixed $action
+     * @return void
+     */
+    public static function delete($route, $action)
+    {
+        self::$routes['delete'][$route] = $action;
+    }
+
+    /**
+     * resolve route 
      * @return void
      */
     public function resolve()
@@ -84,13 +106,21 @@ class Route
         }
         //404 handler
 
+        /**
+         * Handler Route is A Callback Function 
+         */
         if(is_callable($action)){
             call_user_func_array($action, [$this->request->options()]);
         }
 
+        /**
+         * Handler Route is An Array
+         */
         if(is_array($action)){
             call_user_func_array([new $action[0], $action[1]], [$this->request->options()]);
         }
+
+
         // if(is_string($action)){
         //     $action = str_contains($action, '@') ? explode('@', $action) : new \Exception("string should be contain [ @ ] in between controller class and method");
 
